2701f8f1f9e3dc2abf5447a803c069fc56738437
Eric Wang <ewang1@mit.edu>
2022-12-30 19:09:19 -0600

    COULD BE BROKEN, implementing score calculation

--- poker_bot/bot.cpp
+++ poker_bot/bot.cpp
@@ -23,9 +23,78 @@ vector<string> split(const string &s, char delim) {
 }
 
 void throwError(const string &s) {
-      cout << s << endl;
-      exit(1);
+  cout << s << endl;
+  exit(1);
+}
+
+int getCardRank(string s) {
+  if (s == "2") { return 0; }
+  else if (s == "3") { return 1; }
+  else if (s == "4") { return 2; }
+  else if (s == "5") { return 3; }
+  else if (s == "6") { return 4; }
+  else if (s == "7") { return 5; }
+  else if (s == "8") { return 6; }
+  else if (s == "9") { return 7; }
+  else if (s == "10") { return 8; }
+  else if (s == "j" || s == "J") { return 9; }
+  else if (s == "q" || s == "Q") { return 10; }
+  else if (s == "k" || s == "K") { return 11; }
+  else if (s == "a" || s == "A") { return 12; }
+}
+
+int getSuitRank(string s) {
+  if (s == "c" || s == "C") { return 0; }
+  else if (s == "d" || s == "D") { return 1; }
+  else if (s == "h" || s == "H") { return 2; }
+  else if (s == "s" || s == "S") { return 3; }
+}
+
+bool isStraight(int* valFreq) {
+  // Wheel
+  if (valFreq[12] == 1) {
+    for (int i = 0; i < 4; i++) {
+      if (valFreq[i] != 1) {
+        break;
+      }
     }
+    return true;
+  }
+  // Normal Straight
+  for (int i = 0; i < 13; i++) {
+    if (valFreq[i] == 1) {
+      for (int j = i + 1; j < i + 5; j++) {
+        if (j > 12 || valFreq[j] != 1) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  return false;
+}
+
+bool isFlush(int* suitFreq) {
+  for (int i = 0; i < 4; i++) {
+    if (suitFreq[i] == 5) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int getScoreOfHand(vector<Card> combinedCards) {
+  int ret;
+  int* valFreq = new int[13];
+  int* suitFreq = new int[4]; 
+  for (Card currCard : combinedCards) {
+    int length = currCard.size();
+    valFreq[getCardRank(currCard.substr(0, length - 1))]++;
+    suitFreq[getSuitRank(currCard.substr(length - 1, 1))]++;
+  }
+
+  // CONTINUE IMPLEMENTING HERE
+}
 
 typedef string Card;
 
@@ -70,6 +139,35 @@ class Board {
         cout << " " << cards[i];
       }
       cout << "\n" << "\n";
+
+      // PRINT GENERATED CARDS
+      vector<vector<Card>> generatedCards = generatePossibleThreeCards();
+      cout << "Generating Possible Cards For Board:" << endl;
+      for (vector<Card> innerVec : generatedCards) {
+        cout << "New Card: ";
+        for (Card innerCard: innerVec) {
+          cout << " " << innerCard;
+        }
+        cout << "\n";
+      }
+      cout << "\n";
+
+    }
+
+    vector<vector<Card>> generatePossibleThreeCards() {
+      vector<vector<Card>> ans = vector<vector<Card>>();
+      for (int i = 0; i < NUM_BOARD_CARDS; i++) {
+        for (int j = 0; j < i; j++) {
+          for (int k = 0; k < j; k++) {
+            vector<Card> newVec = vector<Card>();
+            newVec.push_back(cards[i]);
+            newVec.push_back(cards[j]);
+            newVec.push_back(cards[k]);
+            ans.push_back(newVec);
+          }
+        }
+      }
+      return ans;
     }
 };
 
@@ -101,6 +199,32 @@ class Player {
       cout << "Player stack size is: " << fixed << setprecision(2) << stackSize << endl;
       holeCards.printHoleCards();
       cout << "\n";
+
+      // PRINT GENERATED CARDS
+      vector<vector<Card>> generatedCards = generatePossibleTwoCards();
+      cout << "Generating Possible Cards For Player:" << endl;
+      for (vector<Card> innerVec : generatedCards) {
+        cout << "New Card: ";
+        for (Card innerCard: innerVec) {
+          cout << " " << innerCard;
+        }
+        cout << "\n";
+      }
+      cout << "\n";
+    }
+
+    vector<vector<Card>> generatePossibleTwoCards() {
+      vector<vector<Card>> ans = vector<vector<Card>>();
+      int numHoleCards = holeCards.numCards; 
+      for (int i = 0; i < numHoleCards; i++) {
+        for (int j = 0; j < i; j++) {
+          vector<Card> newVec = vector<Card>();
+          newVec.push_back(holeCards.cards[i]);
+          newVec.push_back(holeCards.cards[j]);
+          ans.push_back(newVec);
+        }
+      }
+      return ans;
     }
 };
 
@@ -108,6 +232,7 @@ class Showdown {
   private:
     int numPlayers;
     int numBoards;
+    string type;
 
   public:
     vector<Player> players;
@@ -120,6 +245,22 @@ class Showdown {
       boards = vector<Board>();
     }
 
+    int calculatePlayerBoardScore(Player player, Board board) {
+      if (type == "PLO") {
+        int score = 0;
+        vector<vector<Card>> playerPossibleCards = player.generatePossibleTwoCards();
+        vector<vector<Card>> boardPossibleCards = board.generatePossibleThreeCards();
+        for (vector<Card> playerCards : playerPossibleCards) {
+          for (vector<Card> boardCards : boardPossibleCards) {
+            vector<Card> combinedCards (playerCards);
+            combinedCards.insert(playerCards.end(), boardCards.begin(), boardCards.end());
+            score = max(score, getScoreOfHand(combinedCards));
+          }
+        }
+      }
+      return 0;
+    }
+
     void addBoard(string s) {
       Board newBoard = Board(s);
       boards.push_back(newBoard);
@@ -128,6 +269,14 @@ class Showdown {
     void addPlayer(string s) {
       Player newPlayer = Player(s);
       players.push_back(newPlayer);
+    }
+
+    void setType(string s) {
+      type = s;
+    }
+
+    string getType() {
+      return type;
     }
 
     void setNumBoards(int val) {
@@ -169,6 +318,11 @@ int main () {
   string fileName = "input.txt";
   string currLine;
   ifstream myfile (fileName);
+
+  // capture type
+
+  getline(myfile, currLine);
+  showDown.setType(currLine);
 
   // capture boards
 
